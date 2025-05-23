38f8f3f0f0a5c385dac2bf888e311609902442fc
BhavanaBethi7 <bhavanabethi7@gmail.com>
2025-05-23 04:59:40 -0500

    Delete Updated directory

--- Updated/blockchain/1_deploy_contract.js
+++
@@ -1,8 +0,0 @@
-const Certificate = artifacts.require("Certificate");
-
-module.exports = function (deployer) {
-  deployer.deploy(Certificate)
-    .then(() => {
-      console.log('Contract deployed at:', Certificate.address);
-    });
-}; 
--- Updated/blockchain/2_deploy_minimal.js
+++
@@ -1,8 +0,0 @@
-const Minimal = artifacts.require("Minimal");
-
-module.exports = function (deployer) {
-  deployer.deploy(Minimal)
-    .then(() => {
-      console.log('Minimal contract deployed at:', Minimal.address);
-    });
-}; 
